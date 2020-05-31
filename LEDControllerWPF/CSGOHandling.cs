7406cec37eff11141f7e8c365f5358b1d660d349
Patrick <patrick.lopdrup@gmail.com>
2020-05-31 13:40:32 +0200

    CsgoHandling class. Before ProcessListener class

--- LEDControllerWPF/CSGOHandling.cs
+++ LEDControllerWPF/CSGOHandling.cs
@@ -1,18 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using CSGSI;
 using CSGSI.Nodes;
+using System.Windows.Threading;
 
 namespace LEDControllerWPF
 {
     class CSGOHandling
     {
         private GameStateListener _gsl;
+        private DispatcherTimer _startupTimer;
+        private DispatcherTimer _shutdownTimer;
+        private string _csgoFileName = "csgo";
+        private bool _wasRunning = false;
+
         public CSGOHandling()
+        {
+            // start listening after csgo process to start
+            SetTimer();
+        }
+
+        // every x sec we run our event
+        public void SetTimer()
+        {
+            _startupTimer = new DispatcherTimer();
+            _startupTimer.Tick += new EventHandler(CheckStartEvent);
+            _startupTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            _startupTimer.Start();
+
+            _shutdownTimer = new DispatcherTimer();
+            _shutdownTimer.Tick += new EventHandler(CheckShutdownEvent);
+            _shutdownTimer.Interval = new TimeSpan(0,0,0,1);
+            _shutdownTimer.Start();
+        }
+
+        // checks if the program is running
+        private void CheckStartEvent(Object source, EventArgs e)
+        {
+            if (!_wasRunning && IsCsgoRunning(_csgoFileName))
+            {
+                Console.WriteLine("startet");
+                StartCsgoListening();
+                _startupTimer.Stop();
+                _shutdownTimer.Start();
+            }
+        }
+
+        // check if the program has stopped
+        private void CheckShutdownEvent(Object source, EventArgs e)
+        {
+            if (_wasRunning && !IsCsgoRunning(_csgoFileName))
+            {
+                _wasRunning = false;
+                Console.WriteLine("stoppet");
+                _shutdownTimer.Stop();
+                _startupTimer.Start();
+            }
+        }
+
+        // creating GameStateListener when the game is running
+        private void StartCsgoListening()
         {
             _gsl = new GameStateListener(3000);
             _gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
@@ -20,12 +72,8 @@ namespace LEDControllerWPF
             {
                 Environment.Exit(0);
             }
-
             Console.WriteLine("Listening...");
         }
-
-
-        
 
         public void OnNewGameState(GameState gs)
         {
@@ -40,8 +88,25 @@ namespace LEDControllerWPF
                 Console.WriteLine($"Flash: {flash}");
             }
 
-            Console.WriteLine(gs.Player.Weapons.ActiveWeapon.AmmoClip);
+            Console.WriteLine($"Ammo: {gs.Player.Weapons.ActiveWeapon.AmmoClip}");
         }
+
+        // checks if the process is running and set private bool _wasRunning
+        public bool IsCsgoRunning(string FileName)
+        {
+            Process[] processes = Process.GetProcessesByName(FileName);
+            if (!_wasRunning && processes.Length > 0)
+            {
+                _wasRunning = true;
+            }
+            Console.WriteLine($"kører: {_wasRunning}");
+            return processes.Length > 0;
+        }
+
+        
+
+        
+
 
         private void BombPlant(GameState gs)
         {
@@ -56,5 +121,6 @@ namespace LEDControllerWPF
         {
             return gs.Player.State.Flashed;
         }
+
     }
 }
---
+++ LEDControllerWPF/ProcessListener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace LEDControllerWPF
+{
+    class ProcessListener
+    {
+        public event EventHandler ProgramRunning;
+        private DispatcherTimer _timer;
+        private string _fileName;
+
+        public ProcessListener(string FileName)
+        {
+            _fileName = FileName;
+            SetTimer();
+        }
+
+        public void SetTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Tick += new EventHandler(CheckProgramRunning);
+            _timer.Interval = new TimeSpan(0,0,0,1);
+            _timer.Start();
+        }
+
+        public void CheckProgramRunning(Object sender, EventArgs e)
+        {
+            if (IsProgramRunning(_fileName))
+            {
+                OnProgramRunning();
+            }
+        }
+
+        protected virtual void OnProgramRunning()
+        {
+            if (ProgramRunning != null)
+                ProgramRunning(this, new EventArgs());
+        }
+
+        private bool IsProgramRunning(string FileName)
+        {
+            Process[] processes = Process.GetProcessesByName(FileName);
+            return processes.Length > 0;
+        }
+    }
+}
